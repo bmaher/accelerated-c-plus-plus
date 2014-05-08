4b46b312daefb7aa02e657558f8b3f946d9634e4
Brian Maher <brian.maher@here.com>
2014-05-08 20:16:27 +0200

    Initial Commit

---
+++ accelerated-c-plus-plus/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  accelerated-c-plus-plus
+//
+//  Created by Brian Maher on 08/05/2014.
+//  Copyright (c) 2014 Personal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
