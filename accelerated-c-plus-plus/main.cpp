c32f77f0cbe668cd7b13fec6d9cf99ad4a1f0c8c
Brian Maher <brian.maher@here.com>
2014-05-08 20:17:08 +0200

    First commit of Accelerated C++ project.

--- accelerated-c-plus-plus/main.cpp
+++ accelerated-c-plus-plus/main.cpp
@@ -8,11 +8,9 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[])
+int main()
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
 
