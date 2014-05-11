237025cef08c5a94ce63437cd86abaeeae407bd1
Brian Maher <brian.maher@here.com>
2014-05-11 19:58:40 +0200

    Page 13.

--- accelerated-c-plus-plus/main.cpp
+++ accelerated-c-plus-plus/main.cpp
@@ -7,10 +7,31 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    // ask for the person's name
+    std::cout << "Please enter your first name: ";
+    
+    // read the name
+    std::string name;
+    std::cin >> name;
+
+    // build the greeting
+    const std::string greeting = "Hello, " + name + "!";
+    
+    const std::string spaces(greeting.size(), ' ');
+    const std::string second = "* " + spaces + " *";
+    
+    const std::string first(second.size(), '*');
+    
+    std::cout << std::endl;
+    std::cout << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << "* " << greeting << " *" << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first << std::endl;
     return 0;
 }
 
