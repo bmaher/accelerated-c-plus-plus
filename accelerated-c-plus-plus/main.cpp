1a9925da675a5d852b21e7b8df30acc34c0e9992
Brian Maher <brian.maher@here.com>
2014-05-11 21:30:47 +0200

    Page 34.

--- accelerated-c-plus-plus/main.cpp
+++ accelerated-c-plus-plus/main.cpp
@@ -9,29 +9,60 @@
 #include <iostream>
 #include <string>
 
+// defining which standard library names we use
+using std::cin; using std::cout; using std::endl; using std::string;
+
 int main()
 {
     // ask for the person's name
-    std::cout << "Please enter your first name: ";
+    cout << "Please enter your first name: ";
     
     // read the name
-    std::string name;
-    std::cin >> name;
+    string name;
+    cin >> name;
 
     // build the greeting
-    const std::string greeting = "Hello, " + name + "!";
+    const string greeting = "Hello, " + name + "!";
     
-    const std::string spaces(greeting.size(), ' ');
-    const std::string second = "* " + spaces + " *";
+    // ask for padding
+    cout << "Please enter the amount of padding required: ";
     
-    const std::string first(second.size(), '*');
+    // read the padding
+    int pad;
+    cin >> pad;
     
-    std::cout << std::endl;
-    std::cout << first << std::endl;
-    std::cout << second << std::endl;
-    std::cout << "* " << greeting << " *" << std::endl;
-    std::cout << second << std::endl;
-    std::cout << first << std::endl;
+    // number of rows and columns to write
+    const int rows = pad * 2 + 3;
+    const string::size_type cols = greeting.size() + pad * 2 + 2;
+    
+    // seperate output from input
+    cout << endl;
+    
+    // invariant: we have written r rows so far;
+    for (int r = 0; r != rows; ++r) {
+        string::size_type c = 0;
+        
+        // invariant: we have written c characters so far in the current row
+        while (c != cols) {
+    
+            // write greeting?
+            if (r == pad + 1 && c == pad + 1) {
+                cout << greeting;
+                c += greeting.size();
+            } else {
+                
+                // write border?
+                if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+                    cout << "*";
+                else
+                    cout << " ";
+                ++c;
+            }
+        }
+        
+        cout << endl;
+    }
+    
     return 0;
 }
 
